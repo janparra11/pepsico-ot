18d00a84c55b9e714bab7e7825f3e92608a0c1c1
janis <jan.parra@duocuc.cl>
2025-11-13 18:43:06 -0300

    bitacora: arreglo de los login/logout hacerlos visibles, borar la seccion exclusiva de login/logout dejando todo junto

--- core/apps.py
+++ core/apps.py
@@ -1,19 +1,5 @@
 from django.apps import AppConfig
 
-
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
-
-from django.apps import AppConfig
-
-class CoreConfig(AppConfig):
-    name = "core"
-    def ready(self):
-        from . import signals  # noqa
-
-from django.apps import AppConfig
-
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
--- core/signals.py
+++ core/signals.py
@@ -2,15 +2,14 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Perfil
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.dispatch import receiver
+from .models import SessionLog, AuditLog
 
 @receiver(post_save, sender=User)
 def ensure_profile(sender, instance, created, **kwargs):
     if created:
         Perfil.objects.create(user=instance)
-
-from django.contrib.auth.signals import user_logged_in, user_logged_out
-from django.dispatch import receiver
-from .models import SessionLog, AuditLog
 
 @receiver(user_logged_in)
 def _on_login(sender, request, user, **kwargs):
